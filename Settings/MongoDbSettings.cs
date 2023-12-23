80f4faed44917e6399baaf915c9d3a27ee22a3ff
a-bautista <alex.bautista.ramos.90@gmail.com>
2023-12-22 23:09:20 -0800

    added the mongodb config

--- Settings/MongoDbSettings.cs
+++ Settings/MongoDbSettings.cs
@@ -5,10 +5,13 @@ namespace MYWEBAPI.Settings
         public string Host {get; set;}
         public int Port {get; set;}
 
+        public string User {get; set;}
+        public string Password {get; set;}
+
         public string ConnectionString 
         {
             get {
-                return $"mongodb://{Host}:{Port}";
+                return $"mongodb://{User}:{Password}@{Host}:{Port}";
             }
         }
     }
